924d2b68b6699e9701c8e1fda341d8de96d1ed88
Dennis Adrians <DA@office.fortech.de>
2017-05-03 13:13:55 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component
+({
+  selector: 'single-page-app',
+  template: `<h1>Hello {{name}}</h1>`
+})
+export class AppComponent  
+{ 
+  name = 'Angular 4'; 
+}
